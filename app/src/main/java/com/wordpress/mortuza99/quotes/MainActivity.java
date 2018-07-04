189886d49090e2173b8e0ad8944581a7bb4787af
Mim <mortuzahossain1997@gmail.com>
2018-07-05 01:47:04 +0600

    Done V:0.0.0.1

--- app/src/main/java/com/wordpress/mortuza99/quotes/MainActivity.java
+++ app/src/main/java/com/wordpress/mortuza99/quotes/MainActivity.java
@@ -1,33 +1,103 @@
 package com.wordpress.mortuza99.quotes;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private ShakeListener mShaker;
+
+    private String[] quot = {
+            "If Allah wants to do good to somebody, he afflicts him with trials.",
+            "Doctors can treat you, but only ALLAH can heal you",
+            "Allah is enough",
+            "Have fear of Allah wherever you are",
+            "When things are too hard to handle, retreat & count your blessings instead.",
+            "A chance judge yourself on how close you are to ALLAH… how much do you want Jannah? enough to leave your bed and pray Fajr?",
+            "Allah’s timing is perfect in every matter. We don’t always understand the wisdom behind it. But we have to learn to trust it.",
+            "Allah understands our prayers even when we can’t find the words to say Them.",
+            "Tawakkul is having full faith that ALLAH will take care of you even when things look Impossible.",
+            "Allah knows what is the best for you and when it’s best for you to have it.",
+            "One who remembers ALLAH is never AloneLY",
+            "Being a Muslim is about changing yourself, Not changing Islam.",
+            "Every new breath that ALLAH allows you to take is not just a blessing but also a responsibility.",
+            "Severing your parents in their old age is as good as opening the doors of PARADISE, so don’t miss out.",
+            "When we repair our relationship with Allah, He repairs everything else for us.",
+            "You Cannot Delete you internet history from ALLAH.",
+            "Take everyday as a chance to become a better Muslim.",
+            "Only in ISLAM do the king and peasant bow down together side by side proclaiming God’s greatness.",
+            "Why wish upon a star? when you cam pray to the one who created it.",
+            "The solution to every problem is in SABR (patience) and ISTIGFAAR (seeking forgiveness).",
+            "Born from different mothers skins of all colours come together as brothers . that’s the beauty of Islam.",
+            "When Love is for the sake of ALLAH, It never dies.",
+            "So What if this life isn’t perfect? it’s not jannah. Nouman Ali Khan.",
+            "Men and Women have equal rewards for Their deeds.",
+            "The Worst of our faults is our interest in other people’s Faults.",
+            "Trust Allah when things don’t work out the way you wanted. Allah has something better planned for you.",
+            "Stay close to anything that reminds you of ALLAH",
+            "The overly jealous seek to harm and hurt other, but in the end only harm themselves",
+            "Being Muslim is for all day, Not just 5 time a day.",
+            "No matter how hurt you are, You will always find comfort with ALLAH.",
+            "If you want to destroy any nation without war, make adultery & nudity common in the next generation.",
+            "Worries End When SALAH Begins",
+            "Rather than stressing about things we cannot control, pray to The One in control and find relief.",
+            "Balance Your Dunya (world) around your deen (faith), its all a matter of priorities.",
+            "Bad things in life open your eyes to those things your weren’t paying much attention to before.",
+            "That’s a blessing from Allah Too!",
+            "Who can help you get through your problems But ALLAH",
+            "Be embarrassed to sin in public, don’t be shy to show your faith.",
+            "Your Interview with ALLAH is Coming",
+            "Fill your heart with Eemaan and it will become the post peaceful place on earth.",
+            "Peoples of ALHAMDULILLAH” Don’t Have to Complain",
+            "Indeed, ALLAH does not wrong the people at all, but it is the people who are wronging themselves.",
+            "Faith is Trusting GOD even when you don’t understand his plan.",
+            "If the heart becomes hardend, the eye becomes dry.",
+            "The Quran is for ourselves, not our Shelves.",
+            "Pyayer isn’t For Allah, It’s for you. He doesn’t need us But we need him.",
+            "The Greatest thing a Friend can do for you is bring you closer to ALLAH.",
+            "If you don’t want your kids to hurt others, don’t show them how to do it.",
+            "The greatest Jihad is to battle your own soul. to fight the evil within yourself.",
+            "ALLAH makes the impossible Possible",
+            "Guilt is a gift from ALLAH warning you that what you are doing is violating your soul",
+            "Your prefer the life of this world, while the hereafter is better & more lasting.",
+            "No one besides ALLAH can rescue a sould from Hardship",
+            "Be Patient – For what was written for you was written by the greatest of writers",
+            "There is reward for Kindness to every living thing.",
+            "Be somebody in the eyes of ALLAH, even if you are nobody in the eyes of people."
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Using Custom Fonts
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/custom_fonts.ttf");
-        //textView.setTypeface(typeface);
-        final Vibrator vibe = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+        final TextView quots = (TextView) findViewById(R.id.quots);
 
+        // Added Custom Font And Seting Initial Text
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/custom_fonts.ttf");
+        quots.setTypeface(typeface);
+        quots.setShadowLayer(5, 2, 2,R.color.black);
+        quots.setText(R.string.initial_text);
+
+        // Gating the length of the text array
+        final int numberOfQuots = quot.length;
+        final Random random = new Random();
+
+        final Vibrator vibe = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
         mShaker = new ShakeListener(this);
         mShaker.setOnShakeListener(new ShakeListener.OnShakeListener () {
             public void onShake()
             {
                 vibe.vibrate(100);
-                new AlertDialog.Builder(MainActivity.this)
-                        .setPositiveButton(android.R.string.ok, null)
-                        .setMessage("Shooken!")
-                        .show();
+                // Generating Random Number
+                int position = random.nextInt(numberOfQuots - 1);
+                // Geting the straing for random position and show it in textview
+                quots.setText(quot[position]);
             }
         });
     }
@@ -43,4 +113,5 @@ public class MainActivity extends AppCompatActivity {
         mShaker.pause();
         super.onPause();
     }
+
 }
